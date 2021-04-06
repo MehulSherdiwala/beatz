2dca19dea3b863c6b9371e5d54e98d63b63ee85b
Mehul Sherdiwala <msherdiwala16@gmail.com>
2021-04-06 16:31:04 +0530

    First Page test Completed

---
+++ beatz/beatz/Classes/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace beatz.Classes
+{
+    public class Admin
+    {
+        public int aid { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string phone { get; set; }
+    }
+
+}
---
+++ beatz/beatz/admin.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using beatz.Classes;
+using System.Data.SqlClient;
+
+namespace beatz
+{
+    public partial class admin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [System.Web.Services.WebMethod]
+        public static Admin getAdmins(int aid)
+        {
+            Admin a = new Admin();
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\ASP.Net\beatz\beatz\beatz\App_Data\beatz.mdf;Integrated Security=True");
+
+            con.Open();
+
+            string query = "select * from admin where aid=" + aid;
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            SqlDataReader rd = cmd.ExecuteReader();
+
+            while (rd.Read())
+            {
+                a.aid = Convert.ToInt32(rd["aid"]);
+                a.username = rd["username"].ToString();
+                a.email = rd["email"].ToString();
+                a.password = rd["password"].ToString();
+                a.phone = rd["phone"].ToString();
+            }
+            return a;
+        }
+    }
+}
