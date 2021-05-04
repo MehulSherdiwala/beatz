6d02ae5deb1d55b25ea179d3c21d7864a1bf123c
Mehul Sherdiwala <msherdiwala16@gmail.com>
2021-05-04 16:37:42 +0530

    Home page music queue done

---
+++ beatz/beatz/Classes/queue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace beatz.Classes
+{
+    public class Queue
+    {
+        public string title { get; set; }
+        public string mp3 { get; set; }
+        public string oga { get; set; }
+    }
+}
---
+++ beatz/beatz/music_queue.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using beatz.Classes;
+using System.Data.SqlClient;
+namespace beatz
+{
+    public partial class music_queue : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        [System.Web.Services.WebMethod]
+        public static List<Queue> getQueue()
+        {
+            List<Queue> queue = new List<Queue>();
+            Connection con = new Connection();
+            con.Open();
+
+            string query = "select * from music where deleted_at IS NULL";
+            
+
+            SqlDataReader rd = con.ExecuteDQL(query);
+
+            while (rd.Read())
+            {
+                queue.Add(
+                    new Queue
+                    {
+                        title = rd["music_name"].ToString(),
+                        mp3 = "media/" + rd["src"].ToString(),
+                        oga = ""
+                    }
+                );
+            }
+            return queue;
+        }
+    }
+}
